d24ee885b36f533970aaaf23ec34fd2080c24800
Anson Wang <ansor4@gmail.com>
2025-07-10 11:27:25 -0700

    play around with fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import Footer from "@/app/_components/footer";
 import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Space_Grotesk, Instrument_Sans } from "next/font/google";
 import cn from "classnames";
 import { ThemeSwitcher } from "./_components/theme-switcher";
 
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const space_grotesk = Space_Grotesk({ subsets: ["latin"] });
+const instrument_sans = Instrument_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: `You've reached Anson Wang`,
@@ -42,7 +44,7 @@ export default function RootLayout({
         <meta name="theme-color" content="#000" />
       </head>
       <body
-        className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
+        className={cn(instrument_sans.className, "dark:bg-slate-900 dark:text-slate-400")}
       >
         <ThemeSwitcher />
         <div className="min-h-screen">{children}</div>
